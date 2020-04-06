245d0e4cc0948ceba767610e232092c5979acc1e
yamoonz <yamooonz@gmail.com>
2020-04-06 16:02:57 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+var pets = ["Milo", "Otis", "Garfeild"];
+document.getElementById("demo").innerHTML = pets;
+function myFunction() {
+  pets.push("hali");
+  document.getElementById("demo").innerHTML = pets;
+}
 // Write your solution here!
