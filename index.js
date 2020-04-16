506b6dabea5eb920058d728acd094ecd9121ca2c
samah_mustafa <i.samaaah@gmail.com>
2020-04-16 07:00:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+
+const append = [ "Milo", "Otis", "Garfield", "Odie" ];
+
+append.push();
+
+const prepend = ["Milo", "Otis", "Garfield"];
+prepend.unshift('Odie');
+
+const removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop();
+
+const removeFirst = ["Milo", "Otis", "Garfield"];
+removeFirst.shift();
