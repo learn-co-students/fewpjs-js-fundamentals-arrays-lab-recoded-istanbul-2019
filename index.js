7ba15d66d5698d8621b437dd06fa4f28ad1bb07b
https://learn.co/moutaman <moutaman.sabbagh@gmail.com>
2020-04-26 01:33:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+
+// const pets = ["Milo", "Otis", "Garfield"];
+
+// const append = pets.push("Odie");
+// const prepend = pets.unshift("Odie");
+
+// const removeLast = pets.pop();
+// const removeFirst = pets.shift();
+
+const append = ["Milo", "Otis", "Garfield"];
+append.push("Odie");
+
+const prepend = ["Milo", "Otis", "Garfield"];
+prepend.unshift("Odie");
+
+const removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop();
+
+const removeFirst = ["Milo", "Otis", "Garfield"];
+removeFirst.shift();
+
+
+
+
+
